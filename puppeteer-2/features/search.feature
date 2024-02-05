1375abf4b7a8341f639f78caad7d4ff9326b7fa9
Zvyagintsev Sergey <zvyagintsevsergey@yandex.ru>
2024-02-05 23:29:49 +0300

    Ready HW puppeeter 2 v3

--- puppeteer-2/features/search.feature
+++ puppeteer-2/features/search.feature
@@ -1,30 +1,27 @@
 Feature: Service for Movie tickets order
     
-    Feature:
         Scenario: Zveropolis pozitive
-        Given User is on the page "http://qamid.tmweb.ru/client/index.php"
-        When The user selects the desired day
-        And The user selects the desired movie
-        And The user chooses a location
-        And The user is booking tickets
+        Given User is on page1 "http://qamid.tmweb.ru/client/index.php"
+        When The user selects the desired day1
+        Then The user selects the desired movie1
+        Then The user chooses a location1
+        Then The user is booking tickets1
         Then The user confirms the booking "Получить код бронирования"
     
-    Feature Description:
         Scenario: Mickey Mouse positive
-        Given user is on page "http://qamid.tmweb.ru/client/index.php"
-        When The user selects the desired day
-        And The user selects the desired movie
-        And The user chooses a location
-        And The user is booking tickets
-        Then The user confirms the booking "Получить код бронирования"
+        Given User is on page2 "http://qamid.tmweb.ru/client/index.php"
+        When The user selects the desired day2
+        Then The user selects the desired movie2
+        Then The user chooses a location2
+        Then The user is booking tickets2
+        Then The user confirms the booking2 "Получить код бронирования"
     
-    Feature Description:
         Scenario: Mickey Mouse negative
-        Given user is on page "http://qamid.tmweb.ru/client/index.php"
-        And The user selects the desired day
-        And The user selects the desired movie
-        And The user selects the occupied place
-        And The user has booked tickets
-        And The user has confirmed the booking of tickets
+        Given User is on page3 "http://qamid.tmweb.ru/client/index.php"
+        When The user selects the desired day3
+        Then The user selects the desired movie3
+        Then The user selects the occupied place
+        Then The user has booked tickets
+        Then The user has confirmed the booking of tickets
         Then Get Error "Выбранно Вами место уже занято"
         
--- puppeteer-2/features/step_definitions/search.steps.js
+++ puppeteer-2/features/step_definitions/search.steps.js
@@ -1,7 +1,7 @@
 const puppeteer = require("puppeteer");
 const chai = require("chai");
 const expect = chai.expect;
-const { Given, When, Then, Before, After } = require("cucumber");
+const { Given, When, Then, Before, After } = require("@cucumber/cucumber");
 const { putText, getText, clickElement } = require("../../lib/commands.js");
 
 Before(async function () {
@@ -17,85 +17,83 @@ After(async function () {
   }
 });
 
-Given("user is on page- {string}", async function (string) {
+Given("User is on page1 {string}", async function (string) {
   // Открываем страницу
   return await this.page.goto(`http://qamid.tmweb.ru/client/index.php`, {
     setTimeout: 50000,
   });
 });
-When("The user selects the desired day", async function () {
+When("The user selects the desired day1", async function () {
   // Нужный день
   return await clickElement(this.page, ".page-nav__day:last-child");
 });
-And ("The user selects the desired movie", async function () {
+Then ("The user selects the desired movie1", async function () {
   //нужный фильм
-  return await clickElement(
-    this.page,
-    "body > main > section:nth-child(1) > div.movie-seances__hall > ul > li:nth-child(1) > a"
-  );
+  return await clickElement (
+    this.page,"body > main > section:nth-child(1) > div.movie-seances__hall > ul > li:nth-child(1) > a");
 });
-And ("The user chooses a location", async function () {
+Then ("The user chooses a location1", async function () {
   return await clickElement(this.page, ".buying-scheme__chair_standart");
 });
-And ("The user has booked tickets", async function () {
+Then ("The user is booking tickets1", async function () {
   //
   return await clickElement(this.page, ".acceptin-button");
 });
 
-Then("Is the button get code active", async function (string) {
+Then("The user confirms the booking1", async function (string) {
   const actual = await getText(this.page, ".acceptin-button");
   await expect(actual).contain(
     "Получить код бронирования"
   );
 });
 
-Given("user is on page- {string}", async function (string) {
+Given("User is on page2 {string}", async function (string) {
   return await this.page.goto(`http://qamid.tmweb.ru/client/index.php`, {
     setTimeout: 30000,
   });
 });
-When("The user selects the desired day", async function () {
+When("The user selects the desired day2", async function () {
   return await clickElement(this.page, ".page-nav__day:last-child");
 });
-And ("The user selects the desired movie", async function () {
+Then ("The user selects the desired movie2", async function () {
   return await clickElement(
     this.page,"body > main > section:nth-child(2) > div.movie-seances__hall > ul > li:nth-child(1) > a");
 });
-And ("The user chooses a location", async function () {
+Then ("The user chooses a location2", async function () {
   return await clickElement(this.page, ".buying-scheme__chair_standart");
 });
-And ("The user has booked tickets", async function () {
+Then ("The user is booking tickets2", async function () {
   return await clickElement(this.page, ".acceptin-button");
 });
-Then("Is the button get code active", async function (string) {
+Then("The user confirms the booking2", async function (string) {
   const actual = await getText(this.page, ".acceptin-button");
   await expect(actual).сontain("Получить код бронирования");
 });
 
-Given("user is on page- {string}", async function (string) {
+Given("User is on page3 {string}", async function (string) {
   return await this.page.goto(`http://qamid.tmweb.ru/client/index.php`, {
     setTimeout: 30000,
   });
 });
-When("The user selects the desired day", async function () {
+When("The user selects the desired day3", async function () {
   return await clickElement(this.page, ".page-nav__day:last-child");
 });
-And ("The user selects the desired movie", async function () {
+Then ("The user selects the desired movie3", async function () {
   return await clickElement(
     this.page,
     "body > main > section:nth-child(2) > div.movie-seances__hall > ul > li:nth-child(1) > a"
   );
 });
-And ("The user selects the occupied place", async function () {
+Then ("The user selects the occupied place", async function () {
   return await clickElement(this.page, ".buying-scheme__chair_standart");
 });
-And ("The user has booked tickets", async function () {
+Then ("The user has booked tickets", async function () {
   return await clickElement(this.page, ".acceptin-button");
 });
-And ("The user has confirmed the booking of tickets", async function () {
+Then ("The user has confirmed the booking of tickets", async function () {
   return await clickElement(this.page, ".acceptin-button");
 });
-Then("Is the QR code is not visible", async function (string) {
+Then("Get Error", async function (string) {
   const actual = await getText(this.page, ".ticket__hint");
-  await expect(actual).сontain("Выыбранно Вами место уже забронировано");
+  await expect(actual).сontain("Выбранное Вами место уже забронировано");
 });
